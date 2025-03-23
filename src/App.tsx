5d36e88f8cb2ecc330b77cae06de9db88c2146d6
John Eddy <john.k.eddy.51@gmail.com>
2025-03-23 18:48:10 -0400

    modified App component to follow typescript component style

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import Welcome from './Welcome.tsx'
 import Banner from './Banner.tsx'
 import './App.css'
+import React from 'react'
 
-function App() {
-
-  return (
+const App: React.FC = () => (
     <>
       <div>
         <Welcome />
         <Banner />
       </div>
     </>
-  )
-}
+)
 
 export default App
